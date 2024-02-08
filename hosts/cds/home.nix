b3990b87d92f3bdf13c7b24c87d7b7b762791dd6
Christopher Grainger <chris@amplified.ai>
2024-02-08 11:37:29 +0100

    Nixos setup

--- hosts/cds/home.nix
+++ hosts/cds/home.nix
@@ -5,5 +5,19 @@
 }: {
   home.packages = with pkgs; [
     mas
+    obsidian
   ];
+
+  programs.git = {
+    extraConfig.gpg = {
+      "ssh" = {
+        program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+      format = "ssh";
+    };
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIx4VvOw1EP5oCaWa6OTB9oJxZMkd9gQj8Kwf3lcC5y";
+      signByDefault = true;
+    };
+  };
 }
