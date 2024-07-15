acc0309a23224994b0a5c43655e3e7336ec2af7c
Christopher Grainger <chris@amplified.ai>
2024-07-15 07:28:33 +0200

    Formatting

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -1,27 +1,44 @@
 -- plugins/telescope.lua
 
-local telescope = require('telescope')
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
 
-telescope.setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		vimgrep_arguments = vimgrep_arguments,
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+			},
+		},
+		pickers = {
+			find_files = {
+				-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+			},
+		},
+	},
+})
 
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require("telescope").load_extension, "fzf")
 
 -- Custom function for searching open files
 local function telescope_live_grep_open_files()
-  require('telescope.builtin').live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
+	require("telescope.builtin").live_grep({
+		grep_open_files = true,
+		prompt_title = "Live Grep in Open Files",
+	})
 end
 
 -- Make this function available globally
